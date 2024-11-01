af49e9a9f9e606c7685c0916b21cd534443488dd
silence_zhpf <silence_zhpf@aliyun.com>
2024-11-01 15:27:12 +0800

    🐛 修复语言熟练度错误

--- src/io/new/tui/language.rs
+++ src/io/new/tui/language.rs
@@ -9,7 +9,10 @@ use crate::{
 use colored::Colorize;
 use dialoguer::Confirm;
 
-const LIST: [&str; 5] = ["A", "B", "C", "D", "E"];
+const LIST: [&str; 6] = ["A1", "A2", "B1", "B2", "C1", "C2"];
+const DISPLAY_LIST: [&str; 6] = [
+    "A1(17%)", "A2(33%)", "B1(50%)", "B2(67%)", "C1(83%)", "C2(100%)",
+];
 
 /// 从TUI中获取用户输出的语言选项
 pub(super) fn get_languages_from_tui() -> anyhow::Result<Vec<Language>> {
@@ -19,7 +22,7 @@ pub(super) fn get_languages_from_tui() -> anyhow::Result<Vec<Language>> {
         println!("{}", "添加讲述的语言和熟练度:".green());
         let mut builder = LanguageBuilder::default();
         builder_set!(builder, language, "语言");
-        let index = select_prompt("熟练度", &LIST, 5)?;
+        let index = select_prompt("熟练度", &DISPLAY_LIST, 5)?;
         let fluency = LIST.get(index).unwrap();
         builder.fluency(fluency.to_string());
         let language = builder.build().unwrap();
